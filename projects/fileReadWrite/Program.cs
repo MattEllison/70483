14616c58365382db02b0f80de99112786e654d75
Matthew Ellison <matthewcellison@gmail.com>
2020-01-12 18:45:17 -0600

    added file read write

--- projects/fileReadWrite/Program.cs
+++ projects/fileReadWrite/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.IO;
+using System.Text;
 namespace fileReadWrite
 {
     class Program
@@ -7,6 +8,12 @@ namespace fileReadWrite
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using (var file = new FileStream("newfile.2txt", FileMode.Truncate))
+            {
+                var bytes = Encoding.UTF8.GetBytes("Whats up world 2\n");
+                file.Write(bytes);
+            }
+
         }
     }
 }
