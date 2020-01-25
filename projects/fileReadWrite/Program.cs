615b6f9be4a4e19d2f32c282ef5f20f298d567b3
Matthew Ellison <matthewcellison@gmail.com>
2020-01-25 10:53:33 -0600

    updates

--- projects/fileReadWrite/Program.cs
+++ projects/fileReadWrite/Program.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using System.IO.Compression;
+using System;
 using System.IO;
 using System.Text;
 namespace fileReadWrite
 {
+
+    abstract class AbstractClassWithStaticMethod
+    {
+        public static void TestMe()
+        {
+            Console.WriteLine("test me");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
+        {
+            AbstractClassWithStaticMethod.TestMe();
+            //   UsingFileClass();
+            //UsingStreamWriter();
+        }
+
+        static void UsingFileClass()
+        {
+            File.Create("what.txt");
+            var contents = File.ReadAllText("newfile.txt");
+            Console.WriteLine(contents);
+        }
+        static void UsingStreamWriter()
+        {
+            using (var fs = new StreamWriter("hello.txt"))
+            {
+                fs.Write("Hello World");
+            }
+        }
+        static void UsingFileStram()
         {
             Console.WriteLine("Hello World!");
             using (var file = new FileStream("newfile.2txt", FileMode.Truncate))
@@ -13,7 +42,6 @@ namespace fileReadWrite
                 var bytes = Encoding.UTF8.GetBytes("Whats up world 2\n");
                 file.Write(bytes);
             }
-
         }
     }
 }
---
+++ projects/webrequest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Net;
+using System;
+using System.IO;
+
+namespace webrequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WebRequest request = WebRequest.Create("http://www.google.com");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            var ms = new MemoryStream();
+            dataStream.CopyTo(ms);
+            var buffer = new byte[ms.Length];
+            dataStream.Read(buffer, 0, buffer.Length);
+            var responseFromServer = Encoding.UTF8.GetString(buffer);
+            //StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            //string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+
+
+
+
+
+        }
+    }
+}
