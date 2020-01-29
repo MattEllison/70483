6ffe52453dbde584e6bfe33f1cc46797c042f850
Matthew Ellison <matthewcellison@gmail.com>
2020-01-29 06:45:14 -0600

    stuff

---
+++ projects/compiler/Test.cs
@@ -0,0 +1,12 @@
+namespace System.What
+{
+    public class MattsTest
+    {
+        public MattsTest()
+        {
+            Console.WriteLine("whatwhat");
+        }
+        public int What
+        { get; set; }
+    }
+}
