c002a504ceb488aa3cd23b609d85165d7aad2f14
Matthew Ellison <matthewcellison@gmail.com>
2020-01-31 22:43:04 -0600

    partial

---
+++ projects/partial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace partial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = new Test();
+            temp.Start();
+        }
+    }
+
+    partial class Test
+    {
+        public void Start()
+        {
+            StartPartial();
+
+            Console.WriteLine("Start Final");
+
+        }
+
+        partial void StartPartial();
+
+
+    }
+
+    partial class Test
+    {
+        partial void StartPartial()
+        {
+            Console.WriteLine("start parti");
+        }
+    }
+}
