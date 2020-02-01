d27c487ae76b5f5f251f1304321305b227a98f5e
Matthew Ellison <matthewcellison@gmail.com>
2020-01-31 22:20:25 -0600

    extensionmethods

---
+++ projects/extensions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Wath".RunMe();
+            MyExtensions.RunMe("What");
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    static class MyExtensions
+    {
+        public static void RunMe(this string obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
