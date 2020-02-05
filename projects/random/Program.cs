c417c3eb9bae8002f60743d021ffbd91efb8cd3c
Matthew Ellison <matthewcellison@gmail.com>
2020-02-05 17:52:58 -0600

    reflection updates

---
+++ projects/random/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+//using System.Collections.Generic;
+namespace random
+{
+    class Program
+    {
+        class Test
+        {
+            public int MyProperty { get; set; } = 2;
+        }
+        static void Main(string[] args)
+        {
+            var temp = new
+            {
+                FirstName = "What",
+                LastName = "Ehh"
+            };
+            Console.WriteLine(temp.ToString());
+
+            Test[] list = { new Test(), new Test() };
+            System.Collections.Generic.IEnumerable<Test> linqTest = from x in list where x.MyProperty > 1 select x;
+            foreach (var item in linqTest)
+            {
+                Console.WriteLine(item.MyProperty);
+            }
+        }
+    }
+}
---
+++ projects/reflection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace reflection
+{
+
+    class Test
+    {
+        public string Message { get; set; }
+        public Test(string message)
+        {
+            Message = message;
+        }
+        public void CallMe(string ehh, string ehh2)
+        {
+            Console.WriteLine(ehh);
+            Console.WriteLine(ehh2);
+            Console.WriteLine(Message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = new Test("hey ho");
+            var temp2 = new Test("what what");
+            var test = temp.GetType().GetMethods();
+            temp.GetType().GetMethod("CallMe").Invoke(temp2, new string[] { "test", "test3" });
+
+        }
+    }
+}
